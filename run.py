c1e4d3ba2505862ed884164d7c657948f7201a74
Odmarken Odmark <Elias.odmarkk@gmail.com>
2024-05-15 14:48:02 +0000

    added terminal settings for the game

--- run.py
+++ run.py
@@ -4,3 +4,15 @@ import time
 import sys
 import tty
 import termios
+
+# Terminal settings for the game
+
+def get_key():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
